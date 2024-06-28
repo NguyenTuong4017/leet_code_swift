4f49426567480422cb1731c294b9d61667e13581
NguyenTuong0420 <manhtuonga2bd@Gmail.com>
2024-06-28 17:15:10 +0700

    faster code

--- first_letter_appear_twice.playground/Contents.swift
+++ first_letter_appear_twice.playground/Contents.swift
@@ -1,50 +1,24 @@
 import Foundation
 
-func getKey(dictionary: [String.Element: Int], value: Int) -> Character? {
-    for (key, val) in dictionary {
-        if val == value {
-            return key
-        }
-    }
-    return nil
-}
-
 func repeatedCharacter(_ s: String) -> Character {
-    var count = [String.Element: Int]()
-    var second_index = [String.Element: Int]()
+    var characters = [Character]()
     
     for char in s {
-        count[char, default: 0] += 1
-    }
-        
-    for i in 0 ..< s.count {
-        var index = s.index(s.startIndex, offsetBy: i)
-        
-        if count[s[index]]! >= 2 {
-            
-            for z in i+1 ..< s.count {
-                var zIndex = s.index(s.startIndex, offsetBy: z)
-                
-                if s[zIndex] == s[index] && second_index[s[index]] == nil {
-                    second_index[s[index]] = z
-                    break
-                }
-            }
-            
+        if characters.contains(char) {
+            return char
+        } else {
+            characters.append(char)
         }
     }
-    count
-    let minIndex = second_index.values.min()!
-    let result = getKey(dictionary: second_index, value: minIndex)
- 
-    return result!
+    
+    return " "
 }
     
 
 
 let startTime = Date()
 
-repeatedCharacter("wlsqywxtrjyqrnqwxcbgjxwzvqnrjoljnkcpypnuwxinrqklpvuyzifszwvpalqjsvhbwbvqllhdpshdkhdywzvwdszsepxnhsavavfjpfzhjbechakgzqftkqowbjzgdwogkgyajuyfmxrvlqagunffpncjekpknjxljcnexepkytqwfvwvtrrgjhqlbywxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhafncwlsqywxtrjyqrnqwxcbgjxwzvqnrjoljnkcpypnuwxinrqklpvuyzifszwvpalqjsvhbwbvqllhdpshdkhdywzvwdszsepxnhsavavfjpfzhjbechakgzqftkqowbjzgdwogkgyajuyfmxrvlqagunffpncjekpknjxljcnexepkytqwfvwvtrrgjhqlbywxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhafxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhaf")
+repeatedCharacter("wlsqyxtrjyqrnqwxcbgjxwzvqnrjoljnkcpypnuwxinrqklpvuyzifszwvpalqjsvhbwbvqllhdpshdkhdywzvwdszsepxnhsavavfjpfzhjbechakgzqftkqowbjzgdwogkgyajuyfmxrvlqagunffpncjekpknjxljcnexepkytqwfvwvtrrgjhqlbywxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhafncwlsqywxtrjyqrnqwxcbgjxwzvqnrjoljnkcpypnuwxinrqklpvuyzifszwvpalqjsvhbwbvqllhdpshdkhdywzvwdszsepxnhsavavfjpfzhjbechakgzqftkqowbjzgdwogkgyajuyfmxrvlqagunffpncjekpknjxljcnexepkytqwfvwvtrrgjhqlbywxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhafxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhafwlsqywxtrjyqrnqwxcbgjxwzvqnrjoljnkcpypnuwxinrqklpvuyzifszwvpalqjsvhbwbvqllhdpshdkhdywzvwdszsepxnhsavavfjpfzhjbechakgzqftkqowbjzgdwogkgyajuyfmxrvlqagunffpncjekpknjxljcnexepkytqwfvwvtrrgjhqlbywxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhafncwlsqywxtrjyqrnqwxcbgjxwzvqnrjoljnkcpypnuwxinrqklpvuyzifszwvpalqjsvhbwbvqllhdpshdkhdywzvwdszsepxnhsavavfjpfzhjbechakgzqftkqowbjzgdwogkgyajuyfmxrvlqagunffpncjekpknjxljcnexepkytqwfvwvtrrgjhqlbywxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhafxkcyfycnrwpibuegbhpwsveanpmptxxzvvtuekjhozvfwyqzyaxnrwzldnfpmfmlnbwlalnskphxieqlelkzlekywgojlrfxmbxbrylzsbzrtoyrzwaytdwcahnurijmlydyimcbccnkbbzzvntosmwpifumrjzevzmldxnztdoqmzfmskhrwqaxubjyrdjeopmeukccnfngkxcodcxqnnjmcybllhqbcdwduqlvsbfpqcxaxwgjfhwdpycivdkokrsrvcukuhaf")
 for _ in 1...1000000 {
     // Example computation
     let result = 1 + 1
